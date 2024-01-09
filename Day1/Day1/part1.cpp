228d876c8f64987ef017eb5455a8d108f2ec0a24
chofame97 <chofame@naver.com>
2024-01-09 23:22:34 +0900

    1일차
    변수 선언, 상수, auto

---
+++ Day1/Day1/part1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#define PIE 3.14
+
+using namespace std;
+
+int main() {
+
+
+	/*auto a = 100;
+	auto b = 1.4;
+	auto c = "123";
+
+	cout << c << endl;*/
+
+	//float a = 3 * PIE;
+	//cout << a << endl;
+	/*int aa = 'a';
+	cout << aa << endl;
+
+	char a = 'a';
+	cout << int(a) << endl;*/
+	//double a = 13 % 12;
+	//cout << a << endl;
+
+	//float a = 3.14;
+	//int b = 3.14;
+	//cout << a << b << endl;
+	//char bb[] = { 'a','b','c' };
+	//cout << bb << endl;
+
+	/*
+	std::cout << "Hello, World!" << std::endl;
+
+	int number = 3;
+	std::cout << number << std::endl;
+
+	char string1 = 'a';
+	std::cout << string1 << std::endl;
+
+	std::string m = "hello, world";
+	std::cout << m << std::endl;
+
+	int numbers[10] = {1,2};
+	std::cout << numbers<< std::endl;
+	std::cout << numbers[1] << std::endl;
+
+	char strings[10] = "apple";
+	std::cout << strings[3] << std::endl;
+	
+	*/
+	/*char a[10] = "hellohi";
+	a[0] = 'b';
+	cout << a << "\n";
+	cout << sizeof(a);*/
+
+	//int a = 6;
+	//int b = 3;
+	/*{int a = 5; {
+		cout << a << endl;
+		a = 4;
+		cout << a;
+		}
+	}*/
+
+	//cout <<"a = "<< a <<"\nb = "<< b << endl;
+	//cout <<"a = "<< &a <<"\nb = "<< &b << endl;
+
+	return 0;
+}
